a2ca4a85f2d0f29a122dc1119082cf03dab4a447
Davide Filippi <9254428-Vadied@users.noreply.gitlab.com>
2023-04-07 12:16:31 +0200

    portaraits

---
+++ src/components/character/index.jsx
@@ -0,0 +1,54 @@
+import { Box } from "@mui/material";
+
+import { useAppState } from "../../contexts/stateContext";
+import Text from "../text";
+import ButtonList from "../buttonList";
+import MyButton from "../button";
+
+const CharacterCard = () => {
+  const { character, setSection } = useAppState();
+  const { classChar, name, title, race, level, image } = character;
+
+  return (
+    <>
+      <Text>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "1rem",
+            padding: "2rem 1rem",
+          }}
+        >
+          <Box>
+            Tu sei {name}, {title}
+          </Box>
+          <Box
+            sx={{
+              backgroundImage: `url(/portraits/${image})`,
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              height: "10rem",
+              width: "10rem",
+              borderRadius: "50%",
+              boxShadow: "0px 0px 10px #4A3550"
+            }}
+          ></Box>
+          <Box>{classChar}</Box>
+          <Box>
+            {race} di livello {level}
+          </Box>
+        </Box>
+      </Text>
+      <ButtonList>
+        <MyButton handleClick={() => setSection("welcome")}>
+          Torna alla Home
+        </MyButton>
+      </ButtonList>
+    </>
+  );
+};
+
+export default CharacterCard;
